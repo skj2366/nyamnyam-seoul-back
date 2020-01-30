d598c940b701cfaa85fcef8bdae2691d74575536
DongKeun1 <skj2366@naver.com>
2020-01-30 16:03:54 +0900

    make a project & test

---
+++ src/test/java/com/kjk/nyam/NyamnyamSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjk.nyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyamnyamSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
