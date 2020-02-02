791a9d4197770f13b0b219180fbaabfc14bf9fa5
DongKeun1 <skj2366@naver.com>
2020-02-03 06:45:09 +0900

    make CustomerInfoVO, LikeInfoVO, MenuInfoVO, RestaurantListVO

---
+++ src/main/java/com/kjk/nyam/vo/CustomerInfoVO.java
@@ -0,0 +1,26 @@
+package com.kjk.nyam.vo;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Alias("cui")
+@Data
+public class CustomerInfoVO {
+
+	@NotNull
+	private Integer cuiNum;
+	private String cuiId;
+	private String cuiPwd;
+	private String cuiBirth;
+	private String cuiEmail;
+	private String cuiPhone;
+	private String cuiTrans;
+	private String cuiGrade;
+	private String cuiCredat;
+	private String cuiCretim;
+	private String cuiModdat;
+	private String cuiModtim;
+}
