676ebf2b26ed92a07f80c030742a75fcb1ad6738
DongKeun1 <skj2366@naver.com>
2020-02-03 06:48:47 +0900

    MenuInfoVO Update :

--- src/main/java/com/kjk/nyam/vo/MenuInfoVO.java
+++ src/main/java/com/kjk/nyam/vo/MenuInfoVO.java
@@ -1,6 +1,7 @@
 package com.kjk.nyam.vo;
 
 import org.apache.ibatis.type.Alias;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
@@ -12,9 +13,12 @@ public class MenuInfoVO {
 	private Integer relNum;
 	private String meiName;
 	private Integer meiPrice;
-	private String meiImg1;
-	private String meiImg2;
-	private String meiImg3;
+	private MultipartFile meiImg1;
+	private String meiImg1Name;
+	private MultipartFile meiImg2;
+	private String meiImg2Name;
+	private MultipartFile meiImg3;
+	private String meiImg3Name;
 	private String meiCredat;
 	private String meiCretim;
 	private String meiModdat;
