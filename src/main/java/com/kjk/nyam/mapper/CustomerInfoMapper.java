52e5b2430a38829261efdc688157d820a5589937
DongKeun1 <skj2366@naver.com>
2020-02-03 07:23:44 +0900

    기본 구조 생성

---
+++ src/main/java/com/kjk/nyam/mapper/CustomerInfoMapper.java
@@ -0,0 +1,8 @@
+package com.kjk.nyam.mapper;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+@MapperScan
+public interface CustomerInfoMapper {
+
+}
---
+++ src/main/java/com/kjk/nyam/service/CustomerInfoService.java
@@ -0,0 +1,5 @@
+package com.kjk.nyam.service;
+
+public interface CustomerInfoService {
+
+}
