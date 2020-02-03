d290c5b828778d74f95ea51159dcd8fc629e8dcd
DongKeun1 <skj2366@naver.com>
2020-02-03 09:39:30 +0900

    DB setting

--- src/main/java/com/kjk/nyam/mapper/CustomerInfoMapper.java
+++ src/main/java/com/kjk/nyam/mapper/CustomerInfoMapper.java
@@ -1,8 +1,13 @@
 package com.kjk.nyam.mapper;
 
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
+
+import com.kjk.nyam.vo.CustomerInfoVO;
 
 @MapperScan
 public interface CustomerInfoMapper {
 
+	public List<CustomerInfoVO> selectCUIList();
 }
--- src/main/java/com/kjk/nyam/service/impl/CustomerInfoServiceImpl.java
+++ src/main/java/com/kjk/nyam/service/impl/CustomerInfoServiceImpl.java
@@ -1,10 +1,24 @@
 package com.kjk.nyam.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.kjk.nyam.mapper.CustomerInfoMapper;
 import com.kjk.nyam.service.CustomerInfoService;
+import com.kjk.nyam.vo.CustomerInfoVO;
 
 @Service
 public class CustomerInfoServiceImpl implements CustomerInfoService{
+
+	@Resource
+	private CustomerInfoMapper cuiMapper;
+	
+	@Override
+	public List<CustomerInfoVO> selectCUIList() {
+		return cuiMapper.selectCUIList();
+	}
 
 }
