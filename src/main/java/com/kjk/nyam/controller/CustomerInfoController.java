63c251a4b73a5fe9c1ca80700d1c33f1496f2828
DongKeun1 <skj2366@naver.com>
2020-02-03 09:39:30 +0900

    DB setting

--- src/main/java/com/kjk/nyam/controller/CustomerInfoController.java
+++ src/main/java/com/kjk/nyam/controller/CustomerInfoController.java
@@ -1,8 +1,24 @@
 package com.kjk.nyam.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kjk.nyam.service.impl.CustomerInfoServiceImpl;
+import com.kjk.nyam.vo.CustomerInfoVO;
+
+
+@RestController
 public class CustomerInfoController {
 
+	@Resource
+	private CustomerInfoServiceImpl cuiService;
+	
+	@GetMapping("/cui")
+	public List<CustomerInfoVO> selectCUIList() {
+		return cuiService.selectCUIList();
+	}
 }
--- src/main/java/com/kjk/nyam/service/CustomerInfoService.java
+++ src/main/java/com/kjk/nyam/service/CustomerInfoService.java
@@ -1,5 +1,10 @@
 package com.kjk.nyam.service;
 
+import java.util.List;
+
+import com.kjk.nyam.vo.CustomerInfoVO;
+
 public interface CustomerInfoService {
 
+	public List<CustomerInfoVO> selectCUIList();
 }
