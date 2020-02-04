36d3629e27f0276ef06426425195aeb957d0ee9d
kumjo <kumjo@DESKTOP-C19Q34A>
2020-02-04 14:40:41 +0900

    themeListServiceImpl 어노테이션 추가

--- src/main/java/com/kjk/nyam/service/impl/ThemeListServiceImpl.java
+++ src/main/java/com/kjk/nyam/service/impl/ThemeListServiceImpl.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import com.kjk.nyam.mapper.ThemeListMapper;
 import com.kjk.nyam.service.ThemeListService;
 import com.kjk.nyam.vo.ThemeListVO;
 
+@Service
 public class ThemeListServiceImpl implements ThemeListService {
 
 	@Resource
