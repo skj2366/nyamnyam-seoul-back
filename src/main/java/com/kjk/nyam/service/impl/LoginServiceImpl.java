626bdcd18a762ad7dd586414157a5aeb7cdd8f42
DongKeun, Kim <tutgame2089@gmail.com>
2020-02-05 08:51:47 +0900

    Make Login Controller Service Mapper Xml

---
+++ src/main/java/com/kjk/nyam/service/impl/LoginServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kjk.nyam.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.kjk.nyam.mapper.LoginMapper;
+import com.kjk.nyam.service.LoginService;
+import com.kjk.nyam.vo.CustomerInfoVO;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+	@Resource
+	private LoginMapper loginMapper;
+	
+	public CustomerInfoVO doLogin(CustomerInfoVO cui) {
+//		if(cui.getCuiPwd()==null || cui.getCuiPwd().isEmpty()) {
+//			throw new ServiceException("비밀번호를 확인해주세요.");
+//		}
+		return loginMapper.selectForCUI(cui);
+	}
+}
