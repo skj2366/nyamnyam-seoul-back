d470dbc5d25e991e6b902fad22842f935ca4250a
DongKeun1 <skj2366@naver.com>
2020-02-09 14:58:16 +0900

    .

--- src/main/java/com/kjk/nyam/controller/CustomerInfoController.java
+++ src/main/java/com/kjk/nyam/controller/CustomerInfoController.java
@@ -1,6 +1,7 @@
 package com.kjk.nyam.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kjk.nyam.service.CustomerInfoService;
@@ -26,6 +28,17 @@ public class CustomerInfoController {
 	@GetMapping("/cui")
 	public List<CustomerInfoVO> selectCUIList() {
 		return cuiService.selectCUIList();
+	}
+	
+	@GetMapping("/cui/byemail")
+	public CustomerInfoVO selectCUIByEmail(@RequestParam("cuiEmail") String cui) {
+		System.out.println(cui);
+		return cuiService.selectCUIByEmail(cui);
+	}
+	
+	@GetMapping("/cui/cuc/email")
+	public Map<String, Object> selectCUIandCUCByEmail(@RequestParam("email") String email) {
+		return null;
 	}
 	
 	@PostMapping("/cui")
--- src/main/java/com/kjk/nyam/service/CustomerInfoService.java
+++ src/main/java/com/kjk/nyam/service/CustomerInfoService.java
@@ -7,6 +7,7 @@ import com.kjk.nyam.vo.CustomerInfoVO;
 public interface CustomerInfoService {
 
 	public List<CustomerInfoVO> selectCUIList();
+	public CustomerInfoVO selectCUIByEmail(String cui);
 	public Integer insertCUIOne(CustomerInfoVO cui);
 	public Integer updateCUIOne(CustomerInfoVO cui);
 	public Integer deleteCUIOne(int cuiNum);
