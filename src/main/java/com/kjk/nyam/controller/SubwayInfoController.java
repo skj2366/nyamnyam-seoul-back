bb3450770bf03c2e068141281a47b3b64c259383
ES09 <e.song925@gmail.com>
2020-02-10 16:54:29 +0900

    CrossOrigin 추가

--- src/main/java/com/kjk/nyam/controller/SubwayInfoController.java
+++ src/main/java/com/kjk/nyam/controller/SubwayInfoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kjk.nyam.service.SubwayInfoService;
 import com.kjk.nyam.vo.SubwayInfoVO;
 
+@CrossOrigin("http://localhost:80")
 @RestController
 public class SubwayInfoController {
 
@@ -20,7 +22,7 @@ public class SubwayInfoController {
 	@GetMapping("/sui")
 	public List<SubwayInfoVO> selectSUIList() {
 		return suiService.selectSUIList();
-	}
+	}	
 	
 	@GetMapping("/sui/{zoneNum}")
 	public List<SubwayInfoVO> selectSUIListByZone(@PathVariable("zoneNum") int zoneNum){
