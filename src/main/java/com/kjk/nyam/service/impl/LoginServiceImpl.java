49d4cf5246bddf6c53366b829d00eb8d5a278985
DongKeun1 <skj2366@naver.com>
2020-02-10 17:34:09 +0900

    login tokken 적용

--- src/main/java/com/kjk/nyam/service/impl/LoginServiceImpl.java
+++ src/main/java/com/kjk/nyam/service/impl/LoginServiceImpl.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.kjk.nyam.auth.JWTUtil;
 import com.kjk.nyam.auth.SHAEncoder;
 import com.kjk.nyam.mapper.LoginMapper;
 import com.kjk.nyam.service.LoginService;
@@ -14,7 +15,10 @@ public class LoginServiceImpl implements LoginService {
 
 	@Resource
 	private LoginMapper loginMapper;
+	@Resource
+	private JWTUtil jwt;
 	
+	@Override
 	public CustomerInfoVO doLogin(CustomerInfoVO cui) {
 //		if(cui.getCuiPwd()==null || cui.getCuiPwd().isEmpty()) {
 //			throw new ServiceException("비밀번호를 확인해주세요.");
@@ -22,6 +26,10 @@ public class LoginServiceImpl implements LoginService {
 		if(cui.getCuiPwd()!=null) {
 			cui.setCuiPwd(SHAEncoder.encode(cui.getCuiPwd()));
 		}
-		return loginMapper.selectForCUI(cui);
+		CustomerInfoVO cuii = loginMapper.selectForCUI(cui);
+		if(cuii!=null) {
+			cuii.setTokken(jwt.makeJWT(cuii));
+		}
+		return cuii;
 	}
 }
