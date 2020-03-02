8bd3549d75a53a7a74b8e31eaf77f85ff5288d22
DongKeun1 <skj2366@naver.com>
2020-03-03 00:15:53 +0900

    ...

--- src/main/java/com/kjk/nyam/controller/ImageTestController.java
+++ src/main/java/com/kjk/nyam/controller/ImageTestController.java
@@ -1,22 +1,33 @@
 package com.kjk.nyam.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.kjk.nyam.config.FileUtil;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +38,9 @@ import lombok.extern.slf4j.Slf4j;
 //@PropertySource()
 public class ImageTestController {
 
+	@Resource
+	private FileUtil fu;
+	
 	@PostMapping("/it")
 	public Integer insertImage(@RequestParam MultipartFile upload) {
 		String fileName = upload.getOriginalFilename();
@@ -46,7 +60,8 @@ public class ImageTestController {
 //	@Value("${image.upload.uri}")
 //	String uploadUri;
 	
-	String uploadPath = "D:\\project\\nyamnyam_seoul\\back\\nyamnyam-seoul-back\\src\\main\\webapp\\resources\\upload\\";
+//	String uploadPath = "D:\\project\\nyamnyam_seoul\\back\\nyamnyam-seoul-back\\src\\main\\webapp\\resources\\upload\\"; // 동근이 데스크탑
+	String uploadPath = "E:\\study\\workspace\\nyamnyam-seoul\\src\\main\\webapp\\resources\\upload\\"; // 동근이 그램17
 	String uploadUri = "/resources/upload/";
 	
 	@PostMapping("/image/upload")
@@ -72,7 +87,7 @@ public class ImageTestController {
 		System.out.println(imgUrl);
 		// ckeditor upload callback
 		StringBuffer sb = new StringBuffer();
-		sb.append("<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction(");
+		sb.append("<script type='application/javascript'>window.parent.CKEDITOR.tools.callFunction(");
 		sb.append(callback);
 		sb.append(",'");
 		sb.append(imgUrl);
@@ -80,7 +95,31 @@ public class ImageTestController {
 		System.out.println(sb.toString());
 		
 		StringBuffer ww = new StringBuffer();
-		ww.append("<script type='text/javascript'>alert('asdasd')</script>");
-		return sb.toString();
+		ww.append("<script type='text/javascript' crossorigin='anonymous'>alert('asdasd')</script>");
+//		return sb.toString();
+		return "success";
+	}
+	
+	@PostMapping("/upload/one")
+	public Map<String, Object> upload(MultipartFile upload) throws IllegalStateException, IOException{
+		String reName = fu.fileUpload(upload, uploadPath + File.separator);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("url", "http://localhost:809/image?name="+reName); //not null
+		map.put("uploaded", 1); // allow null
+		map.put("uploadedPercent", 1); // allow null
+		map.put("error", "error"); // allow null
+		return map;
+	}
+	
+	@GetMapping("/image")
+	public ResponseEntity<byte[]> image(@RequestParam(value="name") String name) throws IOException{
+		File file = new File(uploadPath + File.separator + name);
+		FileInputStream in = new FileInputStream(file);
+		byte[] buffer = new byte[in.available()];
+		in.read(buffer);
+		in.close();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		return new ResponseEntity<byte[]>(buffer, headers, HttpStatus.OK);
 	}
 }
