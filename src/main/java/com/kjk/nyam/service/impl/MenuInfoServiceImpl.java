545c9e716c21b716b8facb6d2f70ed4332793137
ES09 <e.song925@gmail.com>
2020-03-04 15:25:56 +0900

    평균가계산 완료 - 변동가는 처리 안함 0원으로 됨

--- src/main/java/com/kjk/nyam/service/impl/MenuInfoServiceImpl.java
+++ src/main/java/com/kjk/nyam/service/impl/MenuInfoServiceImpl.java
@@ -58,7 +58,19 @@ public class MenuInfoServiceImpl implements MenuInfoService {
 			} else if(getPrice.matches(".*~.*")) {
 				String[] cut1 = getPrice.split("~");
 				for(int z=0 ; z<cut1.length ; z++) {
-					System.out.println(cut1[z] + "//");
+					String cutWon = "";
+					if(cut1[z].matches(".*원.*")) {
+						cutWon = cut1[z].substring(0, cut1[z].length()-1);						
+					} else {
+						cutWon = cut1[z];
+					}
+					String[] cutComma = cutWon.split(",");
+					String finalPrice = "";
+					for(int j=0 ; j < cutComma.length ; j++) {
+						finalPrice += cutComma[j];
+					}
+					int price = Integer.parseInt(finalPrice);				
+					priceList.add(price);
 				}
 				
 			} else {
@@ -71,14 +83,16 @@ public class MenuInfoServiceImpl implements MenuInfoService {
 				int price = Integer.parseInt(finalPrice);				
 				priceList.add(price);				
 			}			
-		}		
+		}
+		
+		//합계 - 평균 금액 구하기
 		for (int j=0 ; j < priceList.size() ; j++) {
 			sumPrice += priceList.get(j);
 		}
 		if (priceList.size() != 0) {
 			avgPrice = sumPrice / priceList.size();
 		}		
-		//System.out.println("식당번호 : " + relNum + " ==> 가격계산  : 총 가격 : " + sumPrice + " / 평균 가격 : " + avgPrice);
+		System.out.println("식당번호 : " + relNum + " ==> 가격계산  : 총 가격 : " + sumPrice + " / 갯수 : " + priceList.size() + " / 평균 가격 : " + avgPrice);
 		return avgPrice;
 	}
 
